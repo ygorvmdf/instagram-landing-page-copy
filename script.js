96e31b019a395c02b2849203557e668ed82c51f5
ygorvmdf <ygorvitor.fonseca@gmail.com>
2020-12-17 23:02:11 -0300

    fazendo design responsivo

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 const changeImageClass = () => {
-  const imagesList = document.getElementsByClassName('apresentation-image');
-  for (let image of imagesList) {
-    const imageClasses = Object.values(image.classList);
-    if (imageClasses.includes('selected')) {
-      const nextImage = image.nextElementSibling;
-      image.classList.remove('selected');
-      nextImage.classList.add('selected');
-      return;
+  var interval = setInterval(frame(), 3000);
+
+  function frame() {
+
+    const imagesList = document.getElementsByClassName('apresentation-image');
+    for (let image of imagesList) {
+      const imageClasses = Object.values(image.classList);
+      if (imageClasses.includes('selected')) {
+        const nextImage = image.nextElementSibling;
+        image.classList.remove('selected');
+        nextImage.classList.add('selected');
+        return;
+      }
     }
   }
 }
 
-window.setInterval(changeImageClass(), 3000);
+changeImageClass();
